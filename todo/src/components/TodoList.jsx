0b38d6bf6977585b53abe109125525626fe3f9f9
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-05-01 00:34:00 +1000

    Complete Todo component
    Edit, Delete functions

--- todo/src/components/TodoList.jsx
+++ todo/src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -14,12 +15,40 @@ function TodoList() {
         setTodos(newTodo);
 
 
+    };
+
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return ;
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ?newValue : item)))
+
     }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    };
+
+    
+
+    const completeTodo = id => {
+        let updateTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.iscomplete;
+            }
+            return todo;
+        });
+        setTodos(updateTodos);
+    };
 
     return (
         <div>
             <h1>What's the plan for Today?</h1>
             <TodoForm  onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
             
         </div>
     )
